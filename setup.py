94cc80a8950e8a855901984f8e79b733f2c5e69a
Bradley Cicenas <bradley@vektor.nyc>
2021-07-21 16:47:08 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,33 @@
-import os
 import sys
+from os import path
 from setuptools import setup
 
-exec(open('dsplice/version.py').read())
+pwd = path.abspath(path.dirname(__file__))
+rf = lambda x: open(path.join(pwd, x), encoding='utf-8').read()
 
-setup(name='dsplice',
-      version=version,
-      packages=['dsplice'],
-      description='Docker image merge tool',
-      author='Bradley Cicenas',
-      author_email='bradley@vektor.nyc',
-      url='https://github.com/bcicen/dsplice',
-      install_requires=['docker-py>=1.7.2'],
-      license='http://opensource.org/licenses/MIT',
-      classifiers=(
-          'License :: OSI Approved :: MIT License ',
-          'Natural Language :: English',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3',
-      ),
-      keywords='docker image merge devops',
-      entry_points = {
-        'console_scripts' : ['dsplice = dsplice.cli:main']
-      }
-      )
+exec(rf('dsplice/version.py'))
+
+setup(
+  name='dsplice',
+  version=version,
+  packages=['dsplice'],
+  description='Docker image merge tool',
+  long_description=rf('README.md'),
+  long_description_content_type='text/markdown',
+  author='Bradley Cicenas',
+  author_email='bradley@vektor.nyc',
+  url='https://github.com/bcicen/dsplice',
+  install_requires=['docker-py>=1.7.2'],
+  license='http://opensource.org/licenses/MIT',
+  classifiers=(
+    'License :: OSI Approved :: MIT License ',
+    'Natural Language :: English',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+  ),
+  keywords='docker image merge devops',
+  entry_points = {
+    'console_scripts' : ['dsplice = dsplice.cli:main']
+  }
+)
